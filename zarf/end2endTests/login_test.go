b6ef1785be8749a70792368e0eb700302be3a48c
Andres Bott <contact@andresbott.com>
2025-11-16 12:38:14 +0100

    wip

--- zarf/end2endTests/login_test.go
+++ zarf/end2endTests/login_test.go
@@ -1,13 +1,24 @@
 package end2endTests
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLogin(t *testing.T) {
 	finApp, err := new()
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
-	_ = finApp
-	t.Log(finApp)
+
+	err = finApp.StartBackground()
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	t.Log("App started in background")
+
+	err = finApp.Stop()
+	if err != nil {
+		t.Logf("error while stopping %v", err)
+	}
 
 }
