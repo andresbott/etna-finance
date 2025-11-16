1e09dc4ebddb8e861d7f91ed7923968ede3a5831
Andres Bott <contact@andresbott.com>
2025-11-16 12:38:14 +0100

    wip

--- zarf/end2endTests/app.go
+++ zarf/end2endTests/app.go
@@ -1,14 +1,23 @@
 package end2endTests
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"io"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"time"
 )
 
 type app struct {
-	shutdown chan struct{}
-	path     string
+	shutdown    chan struct{}
+	projectPath string
+
+	backendCmd *exec.Cmd
 }
 
 func new() (*app, error) {
@@ -19,14 +28,10 @@ func new() (*app, error) {
 	}
 
 	a := app{
-		shutdown: make(chan struct{}),
-		path:     path,
+		shutdown:    make(chan struct{}),
+		projectPath: path,
 	}
 	return &a, nil
-}
-
-func (app app) StartBackground() {
-
 }
 
 // rootPath returns the root of the project
@@ -35,36 +40,122 @@ func rootPath() (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("unable to get current directory: %w", err)
 	}
-	pwd = filepath.Join(filepath.Dir(pwd), "../..")
+
+	file := "go.mod"
+	// check if we are in the root of the dir
 	abs, err := filepath.Abs(pwd)
 	if err != nil {
 		return "", fmt.Errorf("unable to get absolute path: %w", err)
 	}
+	abs = filepath.Clean(abs)
+	if fileExists(filepath.Join(pwd, file)) {
+		return abs, nil
+	}
 
-	return abs, nil
+	// check if we are in zarf dir
+	abs = filepath.Join(abs, "..")
+	abs, err = filepath.Abs(abs)
+	if err != nil {
+		return "", fmt.Errorf("unable to get absolute path: %w", err)
+	}
+	abs = filepath.Clean(abs)
+	if fileExists(filepath.Join(abs, file)) {
+		return abs, nil
+	}
+
+	// check if we are in end2endTests dir
+	abs = filepath.Join(abs, "..")
+	abs, err = filepath.Abs(abs)
+	if err != nil {
+		return "", fmt.Errorf("unable to get absolute path: %w", err)
+	}
+	abs = filepath.Clean(abs)
+	if fileExists(filepath.Join(abs, file)) {
+		return abs, nil
+	}
+	return "", fmt.Errorf("unable to find project root directory, last checked %s", abs)
 }
 
-func startBackend() {
-	//app := "echo"
-	//
-	//arg0 := "-e"
-	//arg1 := "Hello world"
-	//arg2 := "\n\tfrom"
-	//arg3 := "golang"
-	//
-	//cmd := exec.Command(app, arg0, arg1, arg2, arg3)
-	//stdout, err := cmd.Output()
-	//
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//	return
-	//}
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false
+	} else {
+		panic(err)
+	}
+}
+
+type saveOutput struct {
+	savedOutput []byte
+}
+
+func (so *saveOutput) Write(p []byte) (n int, err error) {
+	so.savedOutput = append(so.savedOutput, p...)
+	return os.Stdout.Write(p)
+}
+
+func (app *app) StartBackground() error {
+	err := app.startBackend()
+	if err != nil {
+		return err
+	}
+
+	// wait for the processes to start
+	time.Sleep(1 * time.Second)
+	return nil
+
+}
+
+func (app *app) startBackend() error {
+
+	var so saveOutput
+
+	args := []string{"make", "run"}
+	cmd := exec.Command(args[0], args[1:]...)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatalf("could not get stderr pipe: %v", err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalf("could not get stdout pipe: %v", err)
+	}
+
+	//cmd.Stdin = os.Stdin
+	//cmd.Stdout = &so
+	//cmd.Stderr = os.Stderr
+
+	cmd.Dir = app.projectPath
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	go func() {
+		werr := cmd.Wait()
+		if werr != nil {
+			_ = cmd.Process.Kill()
+			spew.Dump(werr)
+
+			panic(werr)
+		}
+	}()
+	app.backendCmd = cmd
+
+	return nil
 }
 
 func startFrontend() {
 
 }
 
-func (app app) Stop() {
-
+func (app *app) Stop() error {
+	err := app.backendCmd.Process.Kill()
+	if err != nil {
+		return err
+	}
+	return nil
 }
